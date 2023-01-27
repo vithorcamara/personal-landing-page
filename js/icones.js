3a38c9a4074786e14235d4e690b062f78abe5481
vithor5 <vitor.camarapd@gmail.com>
2023-01-27 15:38:03 -0300

    Grand Mod #1

---
+++ js/icones.js
@@ -0,0 +1,27 @@
+document.getElementById('icone_wpp').addEventListener('mouseenter', function(){
+    document.getElementById('icone_wpp').src= './imgs/Icon-wpp.png'
+})
+document.getElementById('icone_wpp').addEventListener('mouseout', function(){
+    document.getElementById('icone_wpp').src= './imgs/Icon-wpp-white.png'
+})
+
+document.getElementById('icone_insta').addEventListener('mouseenter', function(){
+    document.getElementById('icone_insta').src= './imgs/Icon-insta.png'
+})
+document.getElementById('icone_insta').addEventListener('mouseout', function(){
+    document.getElementById('icone_insta').src= './imgs/Icon-insta-white.png'
+})
+
+document.getElementById('icone_msg').addEventListener('mouseenter', function(){
+    document.getElementById('icone_msg').src= './imgs/Icon-msg.png'
+})
+document.getElementById('icone_msg').addEventListener('mouseout', function(){
+    document.getElementById('icone_msg').src= './imgs/Icon-msg-white.png'
+})
+
+document.getElementById('icone_linkedin').addEventListener('mouseenter', function(){
+    document.getElementById('icone_linkedin').src= './imgs/Icon-linkedin.png'
+})
+document.getElementById('icone_linkedin').addEventListener('mouseout', function(){
+    document.getElementById('icone_linkedin').src= './imgs/Icon-linkedin-white.png'
+})
